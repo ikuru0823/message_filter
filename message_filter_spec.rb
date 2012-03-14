b7a4bbf44fa2e834497ca53a229be63c496e1345
Ikuru CHIBA <ikuru0823@gmail.com>
2012-03-15 00:15:49 +0900

    the end of 1st iteration

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,11 +2,7 @@ require 'rspec'
 require_relative '../message_filter/message_filter'
 
 describe MessageFilter, 'with argument "foo"'do
-   subject{MessageFilter.new('foo')}
-  it{ 
-    should be_detect('hello from foo')
-  }
-  it{
-    should_not be_detect('hello,world!')
-  }
+  subject{MessageFilter.new('foo')}
+  it{should be_detect('hello from foo')}
+  it{should_not be_detect('hello,world!')}
 end
