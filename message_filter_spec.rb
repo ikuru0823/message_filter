67108263e379db1201f4578381087b9d61f2048d
Ikuru CHIBA <ikuru0823@gmail.com>
2012-03-14 22:53:54 +0900

    code example

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,4 +2,8 @@ require 'rspec'
 require_relative '../message_filter/message_filter'
 
 describe MessageFilter do
+  it 'shold detect message with NG word' do
+    filter = MessageFilter.new('foo')
+    filter.detect?('hello from foo').shold == true
+  end
 end
