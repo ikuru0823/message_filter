74c880db8c7e4cdd57c5176026d030870615fa0c
Ikuru CHIBA <ikuru0823@gmail.com>
2012-03-14 23:25:57 +0900

    be_xxx

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -6,9 +6,9 @@ describe MessageFilter do
     @filter = MessageFilter.new('foo')
   end
   it 'should detect message with NG word' do
-    @filter.detect?('hello from foo').should == true
+    @filter.should be_detect('hello from foo')
   end
   it 'should not detect message without NG word' do
-    @filter.detect?('hello,world').should == false
+    @filter.should_not be_detect('hello,world!')
   end
 end
