dbcfabdd1875454f3dd823100f567fb0f7c15c1e
Ikuru CHIBA <ikuru0823@gmail.com>
2012-03-14 23:40:44 +0900

    subjectブロックをインライン化

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,13 +2,11 @@ require 'rspec'
 require_relative '../message_filter/message_filter'
 
 describe MessageFilter, 'with argument "foo"'do
-  before do
-    @filter = MessageFilter.new('foo')
-  end
+   subject{MessageFilter.new('foo')}
   it{ 
-    @filter.should be_detect('hello from foo')
+    should be_detect('hello from foo')
   }
   it{
-    @filter.should_not be_detect('hello,world!')
+    should_not be_detect('hello,world!')
   }
 end
