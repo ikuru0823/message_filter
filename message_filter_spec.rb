5ee6703a9d79cf5853ef622a141db0cfe63d32c3
Ikuru CHIBA <ikuru0823@gmail.com>
2012-03-14 23:08:32 +0900

    triangulation

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,8 +2,12 @@ require 'rspec'
 require_relative '../message_filter/message_filter'
 
 describe MessageFilter do
-  it 'shold detect message with NG word' do
+  it 'should detect message with NG word' do
     filter = MessageFilter.new('foo')
     filter.detect?('hello from foo').should == true
+  end
+  it 'should not detect message without NG word' do
+    filter = MessageFilter.new('foo')
+    filter.detect?('hello,world').should == false
   end
 end
